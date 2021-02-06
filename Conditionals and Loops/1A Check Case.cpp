31461747801fb2863734e120fb873a99f74a6fa8
Harsh Rajput <harshit.x9@gmail.com>
2021-02-06 11:23:56 +0530

    Some Changes!

--- Conditionals and Loops/1A Check Case.cpp
+++ Conditionals and Loops/1A Check Case.cpp
@@ -3,12 +3,10 @@ using namespace std;
 
 int main() {
     char c;
-    int n;
     cin>>c;
-    n=c;
-    if(n>=97 && n<=122 ){
-        cout << 0;  
-    } else if(n>=65 && n<=90){
+    if(c>='a' && c<='z' ){
+        cout << 0;
+    } else if(c>='A' && c<='Z'){
         cout << 1;
     } else{
         cout<<-1;
